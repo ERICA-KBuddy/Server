b06b87ea2901ca018a1b9ead8d7e46884515b763
bnbong <bbbong9@gmail.com>
2024-06-02 01:29:54 +0900

    [FEAT] 장소 큐레이션 로직 완성

--- src/db/data/csv_converter.py
+++ src/db/data/csv_converter.py
@@ -1,3 +1,4 @@
+import random
 import chardet
 import pandas as pd
 
@@ -6,6 +7,35 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from src.db.models import Area
 from src.db import database
 from src.schemas.requests import AreaCreate
+from enum import Enum
+
+
+class AreaName(Enum):
+    강남구 = "Gangnam-gu"
+    강동구 = "Gangdong-gu"
+    강북구 = "Gangbuk-gu"
+    강서구 = "Gangseo-gu"
+    관악구 = "Gwanak-gu"
+    광진구 = "Gwangjin-gu"
+    구로구 = "Guro-gu"
+    금천구 = "Geumcheon-gu"
+    노원구 = "Nowon-gu"
+    도봉구 = "Dobong-gu"
+    동대문구 = "Dongdaemun-gu"
+    동작구 = "Dongjak-gu"
+    마포구 = "Mapo-gu"
+    서대문구 = "Seodaemun-gu"
+    서초구 = "Seocho-gu"
+    성동구 = "Seongdong-gu"
+    성북구 = "Seongbuk-gu"
+    송파구 = "Songpa-gu"
+    양천구 = "Yangcheon-gu"
+    영등포구 = "Yeongdeungpo-gu"
+    용산구 = "Yongsan-gu"
+    은평구 = "Eunpyeong-gu"
+    종로구 = "Jongno-gu"
+    중구 = "Jung-gu"
+    중랑구 = "Jungnang-gu"
 
 
 async def create_data(db: AsyncSession, data: AreaCreate):
@@ -39,6 +69,27 @@ async def csv_to_db(db: AsyncSession):
             contact_num=str(row['연락처']),
         )
         await create_data(db, data)
+
+
+async def get_top_5_areas(file_path: str):
+    encoding = detect_encoding(file_path)
+    df = pd.read_csv(file_path, encoding=encoding)
+
+    df['기초지자체명'] = df['기초지자체명'].str.strip()
+
+    df = df.dropna(subset=['기초지자체 방문자 수'])
+
+    top_5_areas = df.sort_values(by='기초지자체 방문자 수', ascending=False).head(5)
+
+    top_5_areas['기초지자체명_영어'] = top_5_areas['기초지자체명'].map(
+        lambda x: AreaName[x].value if x in AreaName.__members__ else x)
+
+    return top_5_areas['기초지자체명_영어'].tolist()
+
+
+async def get_random_area(file_path: str):
+    top_5_areas = await get_top_5_areas(file_path)
+    return random.choice(top_5_areas)
 
 
 if __name__ == "__main__":
